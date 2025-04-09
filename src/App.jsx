ddb85542311f5dfbb7b7bedf9f3f59db258db79f
Thanveer <thanveerppvy@gmail.com>
2025-04-09 23:54:26 +0530

    todo List compleated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './components/ToDo'
+
+const App = () => {
+  return (
+    <div className='min-h-screen grid	bg-gradient-to-r from-sky-500 to-indigo-500	'>
+
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
